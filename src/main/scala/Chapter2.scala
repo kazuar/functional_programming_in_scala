132f0ee095cd2dc39606ec61765fe77c2ef1c64d
Tzahi Vidas <tzahi.vidas@gmail.com>
2016-05-17 23:06:00 -0400

    Exercise 2.2 - isSorted

--- src/main/scala/Chapter2.scala
+++ src/main/scala/Chapter2.scala
@@ -3,10 +3,9 @@
   */
 object Chapter2 {
 
-  // 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
+  // 2.1 Fibonacci function - 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
   def fib(n: Int): Int = {
     def go(n: Int, acc: Int): Int = {
-      println(n + " " + acc)
       if (n <= 0) 0
       else if (n == 1) 1
       else go(n - 1, acc) + go(n - 2, acc)
@@ -15,8 +14,37 @@ object Chapter2 {
     go(n, 1)
   }
 
+  def isMatch(a: Int): Boolean = false
+  def findFirst[A](as: Array[A], p: A => Boolean): Int = {
+    def loop(n: Int): Int =
+      if (n >= as.length) -1
+      else if (p(as(n))) n
+      else loop(n + 1)
+
+    loop(0)
+  }
+
+  // 2.2 isSorted - checks whether an Array[A] is sorted
+  // according to a given comparison function
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    def loop(n: Int): Boolean = {
+      if (n + 1 >= as.length) true
+      else if (!ordered(as(n), as(n + 1))) false
+      else loop(n + 1)
+    }
+
+    loop(0)
+  }
+
   def main(args: Array[String]): Unit = {
-    val res = fib(9)
+//    val res = fib(9)
+//    println(res)
+
+//    val res = findFirst(a, isMatch)
+
+    val a = Array(1,2,3)
+    def isOrdered(a: Int, b: Int): Boolean = a <= b
+    val res = isSorted(a, isOrdered)
     println(res)
   }
 }
