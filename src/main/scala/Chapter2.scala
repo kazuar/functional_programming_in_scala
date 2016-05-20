2499b3da46a446383e39b55a405d1eed7b06cac5
Tzahi Vidas <tzahi.vidas@gmail.com>
2016-05-19 22:12:12 -0400

    Finish 2.5

--- src/main/scala/Chapter2.scala
+++ src/main/scala/Chapter2.scala
@@ -45,4 +45,9 @@ object Chapter2 {
   def uncurry[A, B, C](f: A => B => C): (A, B) => C = {
     (a: A, b: B) => f(a)(b)
   }
+
+  // 2.5 compose
+  def compose[A, B, C](f: B => C, g: A => B): A => C = {
+    (a: A) => f(g(a))
+  }
 }
