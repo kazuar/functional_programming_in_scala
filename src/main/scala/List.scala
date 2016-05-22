0e3d41024d8a346b0d3637147dbfa9a452b314d9
Tzahi Vidas <tzahi.vidas@gmail.com>
2016-05-22 14:36:46 -0400

    foldRight implementation

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -53,6 +53,17 @@ object List {
     case Cons(h, t) => Cons(h, init(t))
   }
 
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  def sum2(ns: List[Int]) =
+    foldRight(ns, 0)((x, y) => x + y)
+
+  def product2(ns: List[Double]) =
+    foldRight(ns, 1.0)(_ * _)
+
   def apply[A](as: A*): List[A] =
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
