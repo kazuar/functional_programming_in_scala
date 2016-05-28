1735e3212993356067da99191d33648b56f9d212
Tzahi Vidas <tzahi.vidas@gmail.com>
2016-05-28 10:21:45 -0400

    Exercise 3.28

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -19,5 +19,17 @@ object Tree {
     case Leaf(v) => v
     case Branch(left, right) => maximum(left) max maximum(right)
   }
+
+  // 3.27
+  def depth[A](tree: Tree[A]): Int = tree match {
+    case Leaf(v) => 1
+    case Branch(left, right) => (1 + depth(left)) max (1 + depth(right))
+  }
+
+  // 3.28
+  def map[A,B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+  }
 }
 
--- src/test/scala/Tree$Test.scala
+++ src/test/scala/Tree$Test.scala
@@ -17,4 +17,17 @@ class Tree$Test extends FlatSpec with MustMatchers {
     Tree.maximum(Branch(Branch(Leaf(2), Leaf(3)), Branch(Leaf(10), Leaf(5)))) must equal(10)
   }
 
+  it must "calculate the depth" in {
+    Tree.depth(Leaf(2)) must equal(1)
+    Tree.depth(Branch(Leaf(2), Branch(Leaf(1), Leaf(100)))) must equal(3)
+    Tree.depth(
+      Branch(Branch(Leaf(1), Branch(Leaf(3), Branch(Branch(Leaf(10), Branch(Leaf(4), Leaf(9))), Leaf(7)))), Leaf(5))
+    ) must equal(7)
+  }
+
+  it must "map over elements in a tree" in {
+    Tree.map(Leaf(2))((x) => x + 1) must equal(Leaf(3))
+    Tree.map(Branch(Leaf(2), Leaf(4)))((x) => x * 2) must equal(Branch(Leaf(4), Leaf(8)))
+  }
+
 }
