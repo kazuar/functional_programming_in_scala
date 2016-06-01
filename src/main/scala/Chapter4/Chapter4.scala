b91807e3eae412997c49d9df4b182b6fb83fa003
Tzahi Vidas <tzahi.vidas@gmail.com>
2016-05-31 22:06:54 -0400

    Exercise 4.2

---
+++ src/main/scala/Chapter4/Chapter4.scala
@@ -0,0 +1,14 @@
+package Chapter4
+
+/**
+  * Created by vidas on 5/31/16.
+  */
+object Chapter4 {
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+}
